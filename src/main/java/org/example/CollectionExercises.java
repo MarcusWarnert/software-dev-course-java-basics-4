98d3be74844f505a19a031e822737ef3a084e824
MarcusWarnert <mrvwarnert@gmail.com>
2026-01-03 20:42:41 -0600

    Added example exercise and improved instructions.

--- src/main/java/org/example/CollectionExercises.java
+++ src/main/java/org/example/CollectionExercises.java
@@ -11,15 +11,15 @@ public class CollectionExercises {
     }
 
     public String[] makeFruitStringArrayWithSize(int size) {
-        String[] fruits = {"apple", "banana", "cherry", "date", "elderberry"};
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+        String[] fruits = new String[size];
+        for (int i = 0; i <size; i++) {
+            fruits[i] = "apple";
         }
         return fruits;
     }
 
     public String[] makeTopThreeArray(String[] fruits) {
-        String[] threeFruits = {"apple", "banana", "cherry"};
+        String[] threeFruits = {fruits[0], fruits[1], fruits[2]};
         return threeFruits;
     }
 
@@ -35,9 +35,9 @@ public class CollectionExercises {
 
     public ArrayList<String> makeListOfThreeFruits(String fruit1, String fruit2, String fruit3) {
         ArrayList<String> threeFruitList = new ArrayList<String>();
-        threeFruitList.add("apple");
-        threeFruitList.add("banana");
-        threeFruitList.add("cherry");
+        threeFruitList.add(fruit1);
+        threeFruitList.add(fruit2);
+        threeFruitList.add(fruit3);
         return threeFruitList;
     }
 
@@ -53,21 +53,14 @@ public class CollectionExercises {
 
     public String lookupAppleColor(HashMap<String, String> fruitMap) {
         HashMap<String, String> fruit = new HashMap<String, String>();
-        fruit.put("apple", "red");
-        fruit.put("banana", "yellow");
-        fruit.put("Charlie", "red");
-        fruit.put("date", "brown");
-        fruit.put("elderberry", "black");
-
-        String color = fruit.get("apple");
-        return color;
+        return fruitMap.get("apple");
     }
 
     public HashSet<String> makeFruitSet(String fruit1, String fruit2, String fruit3) {
         HashSet<String> fruit = new HashSet<String>();
-        fruit.add("apple");
-        fruit.add("banana");
-        fruit.add("Charlie");
-            return fruit;
+        fruit.add(fruit1);
+        fruit.add(fruit2);
+        fruit.add(fruit3);
+        return fruit;
     }
 }
